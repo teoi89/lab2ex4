27daeda8c0c6f62ded608bc5ffbde66ef6ef9ce7
teoi89 <teofil.lucaci@yahoo.ro>
2022-12-05 10:59:23 +0200

    Add project files.

---
+++ lab2ex4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace lab2ex4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insert a number in order to find if it is a palindrome or not:");
+            int n, reverse = 0, rem;
+            n = int.Parse(Console.ReadLine());
+            int m = n;
+            while (m != 0)
+            {
+                rem = m % 10;
+                reverse = reverse * 10 + rem;
+                m = m / 10;
+            }
+
+            if (reverse == n)
+            {
+                Console.WriteLine("Your number " + n + " is a palindrome");
+            }
+            else
+            {
+                Console.WriteLine(n + " Is not a palindrome");
+            }
+        }
+    }
+}
